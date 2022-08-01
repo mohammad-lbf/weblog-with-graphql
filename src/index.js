f112b43e3b7a65952214462f356fc6ddfb2a74ad
mohammad_lbf <labbaf2016@gmail.com>
2022-08-01 23:49:59 +0430

    client object repaired

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import App from './App';
 import { ApolloClient , InMemoryCache , ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-    uri="https://api-us-west-2.hygraph.com/v2/cl67rnab4celw01ut8byq83bg/master",
-    cache= new InMemoryCache()
+    uri:"https://api-us-west-2.hygraph.com/v2/cl67rnab4celw01ut8byq83bg/master",
+    cache: new InMemoryCache()
 })
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
