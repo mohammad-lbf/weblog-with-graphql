e569c0cff3e77f144fb1457a75a8bdf770959596
mohammad_lbf <labbaf2016@gmail.com>
2022-08-01 23:37:33 +0430

    apollo client setup done in index.js & provided

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ApolloClient , InMemoryCache , ApolloProvider } from '@apollo/client';
 
+const client = new ApolloClient({
+    uri="https://api-us-west-2.hygraph.com/v2/cl67rnab4celw01ut8byq83bg/master",
+    cache= new InMemoryCache()
+})
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+   <ApolloProvider client={client} >
+        <App />
+   </ApolloProvider>
 );
